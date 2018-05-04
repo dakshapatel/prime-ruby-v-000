f296f3da10f3b111325a211ecffe1143ed6133d2
Daksha Patel <dakshapatel100@gmail.com>
2018-05-04 17:23:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,15 @@
 
 
 
-def prime?(string)
-string.to_a
+def prime?(n)
 
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
 
 end
